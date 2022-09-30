a2c41b6f52392ed8b6cac1518a9bc3e69d792967
Yushua <yushabakker1@gmail.com>
2022-09-30 15:48:29 +0200

    submitting start for unchunking, but first look into chunks

--- webserv/src/message/check.cpp
+++ webserv/src/message/check.cpp
@@ -1,19 +1,43 @@
 #include <message.hpp>
 
+//each msg has their own bool NOW
+
 void message::check()
 {
 	map<string, string>::iterator itr = headers.begin();
 	map<string, string>::iterator end = headers.end();
+	// std::vector<std::string> store;//dont' forget to delete it
+
+
 	for (; itr != end; ++itr)
 	{
 		// itr->first;
 		// itr->second;
+		if (itr->first == "Transfer-Encoding:")
+		{
+			this->unchunk();
+		}
 		std::cout << itr->first << " " << itr->second << std::endl;
 	}
 	std::cout << " " << std::endl;
+
 	valid = true;
 }
 
+void message::unchunk()
+{
+	map<string, string>::iterator itr = headers.begin();
+	map<string, string>::iterator end = headers.end();
+
+	for (; itr != end; ++itr)
+	{
+		// itr->first;
+		// itr->second;
+		if (itr->first == "Transfer-Encoding:")
+			break;
+	}
+	std::cout << "hello i am here\n\n";
+}
 /*
 
 General Header
