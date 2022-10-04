45f47f6958a1f9a6303b1c1db08b87ac02ba585d
LANIK2000 <lanikmail@gmail.com>
2022-10-04 14:53:52 +0200

    Fixed silly mistake in to_string

--- webserv/src/lib/ft_to_string.cpp
+++ webserv/src/lib/ft_to_string.cpp
@@ -3,10 +3,11 @@
 
 using namespace std;
 
-static char static_buffer[20] = "0000000000000000000";
+static char static_buffer[20];
 
 const string ft_to_string(unsigned long n) {
-	char *buffer_ptr = static_buffer + 19;
+	static_buffer[19] = '\0';
+	char *buffer_ptr = static_buffer + 18;
 	
 	while (n > 9) {
 		*buffer_ptr = n % 10 + '0';
