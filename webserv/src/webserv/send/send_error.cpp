5e81c49338a9f5bfe757beb69cea9f3b7eb67a4d
LANIK2000 <lanikmail@gmail.com>
2022-10-18 15:32:37 +0200

    Send_error now actually sends the error code.

--- webserv/src/webserv/send/send_error.cpp
+++ webserv/src/webserv/send/send_error.cpp
@@ -5,9 +5,10 @@
 
 void webserv::send_new_error(const int index, const int error_code) {
 
-	string headers = "HTTP/1.1 200 OK\n";
-	
-	headers += this->header_get_content_type(".html");
+	string headers = "HTTP/1.1 " + ft_to_string(error_code) + '\n';
+	string &error_page = error_pages.at(error_code);
 
-	this->send_new_file(index, headers, error_pages.at(error_code));
+	headers += this->header_get_content_type(error_page);
+
+	this->send_new_file(index, headers, error_page);
 }
