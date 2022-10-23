362404c4ef21434e84108ea1f16f4ee5f667cb67
Yushua <yushabakker1@gmail.com>
2022-10-23 03:35:53 +0200

    overall, the body is empty now, i know there is another fiel wiht everything, but I can only check the body. the file is also not created so I have to look at that as well

--- webserv/src/webserv/HTML_Commands/cmd_POST.cpp
+++ webserv/src/webserv/HTML_Commands/cmd_POST.cpp
@@ -46,11 +46,9 @@ void webserv::cmd_POST(const int index, message &msg) {
 	map<string, string>::iterator end = _header.end();
 
 	bool chunk = false;
-	std::cout << "here I am ===========" << std::endl;
 	for (; itr != end; ++itr){
 		if (itr->first == "Content-Length:"){
 			if (msg.getContentLength() != strlen(msg.getBody())){
-				std::cout << "hey1" << std::endl;
 				this->send_new_error(sockets[index].fd, 404);
 				this->disconnect_socket(index);
 				return;
@@ -61,7 +59,6 @@ void webserv::cmd_POST(const int index, message &msg) {
 			std::string tmp = itr->second;
 			msg.doUnHost(tmp);
 			if (msg.isValid() == false){
-				std::cout << "hey2" << std::endl;
 				this->send_new_error(sockets[index].fd, 404);
 				this->disconnect_socket(index);
 				return;
@@ -69,7 +66,6 @@ void webserv::cmd_POST(const int index, message &msg) {
 		}
 		else if ((itr->first == "Transfer-Encoding:" || itr->first == "TE:" ) && itr->second.find("chunked")){
 			if (unchunkCheck(msg.getBody()) == false){
-				std::cout << "hey3" << std::endl;
 				this->send_new_error(sockets[index].fd, 404);
 				this->disconnect_socket(index);
 				return;
@@ -85,22 +81,29 @@ void webserv::cmd_POST(const int index, message &msg) {
 	
 	what do they mean wiht new resource?
 	*/
+	// if (msg.getPath() == "")
+	// {
+	// }
+	std::cout << "======empty=====" << std::endl;
+	std::cout << msg.getPath() << std::endl;
+	ofstream file;
 	if (stat(msg.getPath().c_str(), &file_info) == -1)
 	{
-		std::cout << "hey4" << std::endl;
-		this->send_new_error(sockets[index].fd, 404);
-		this->disconnect_socket(index);
-		return;
+		//create
+		file.open("root/cgi-bin/file", fstream::in | fstream::out | fstream::trunc);
+		if (!file.good()) {
+			this->send_new_error(sockets[index].fd, 404);
+			this->disconnect_socket(index);
+			return;
+		}
 	}
-	ofstream file;
-	file.open(msg.getPath(), ios::out);
-	if (!file.good()) {
-		std::cout << "hey5" << std::endl;
+	else{//if it does exist, corrupt file, can't override the file
 		this->send_new_error(sockets[index].fd, 404);
 		this->disconnect_socket(index);
 		return;
 	}
 	std::string string = msg.getBody();
+	std::cout << msg.getBody() << std::endl; //nothing in the body yet
 	if (chunk == true){
 		bool chunkStatus = false;
 		std::string tmp;
@@ -110,81 +113,38 @@ void webserv::cmd_POST(const int index, message &msg) {
 		while (i < len){
 			start = i;
 			i = string.find("\r");
-			if (chunkStatus == false){
+			if (chunkStatus == false){//when number
 				if (string.substr(start, i - start) == "0")
 					break;
 				chunkStatus = true;
 			}
-			else if (chunkStatus == true){
+			else if (chunkStatus == true){//when input
 				chunkStatus = false;
+				std::cout << "message[" << string.substr(start, i - start) << "]\n";
 				file << string.substr(start, i - start);
 			}
 			i += 2;
 		}	
 	}
 	else
-	file << msg.getBody();
+		file << msg.getBody();
 	file.close();
+	std::cout << "file uploaded" << std::endl;
 }
 
 /*
-POST /echo/post/json HTTP/1.1
-Authorization: Bearer mt0dgHmLJMVQhvjpNXDyA83vA_Pxh33Y
-Accept: application/json
-Content-Type: application/json
-Content-Length: 85 /do in check
-Host: reqbin.com
+so how does uplaoding work?
 
-{
-   "Id": 12345,
-   "Customer": "John Smith",
-   "Quantity": 1,
-   "Price": 10.00
-}
+if the file does not exist, make it, and then fill it in
+but if the file does exist, delete it ONCE, and then fill it in. or, jsut add to it?
+but how o i know, with chunks. if get multiple chunks, then, oeps, ikeep deleting the file.
+so with chunks, if it is there. I need to, for the first time, delete the file, and then keep adding to it.
+so for each fiel the chunk is set to, I need to also remember if it is the first time.
 
+scenario
 
-The POST method sends data to a server for processing
-Send data to the server for processing.  message bdy
-
-For
-example, consider when you are shopping online at Joe's Hardware and you click on the "submit
-purchase" button. Clicking on the button submits a POST request (discussed later) with your credit
-card information, and an action is performed on the server on your behalf. In this case, the action is
-your credit card being charged for your purchase. 
-
-The POST method was designed to send input data to the server.[3] In practice, it is often used to
-support HTML forms. The data from a filled-in form typically is sent to the server, which then
-marshals it off to where it needs to go (e.g., to a server gateway program, which then processes it).
-Figure 3-10 shows a client making an HTTP request—sending form data to a server—with the POST 
-
-When an HTTP/1.0 client makes a POST request and receives a 302 redirect status code in response, it
-will follow the redirect URL in the Location header with a GET request to that URL (instead of
-making a POST request, as it did in the original request). 
-
-HTTP/1.0 servers expect HTTP/1.0 clients to do this—when an HTTP/1.0 server sends a 302 status
-code after receiving a POST request from an HTTP/1.0 client, the server expects that client to follow
-the redirect with a GET request to the redirected URL. 
-
-The confusion comes in with HTTP/1.1. The HTTP/1.1 specification uses the 303 status code to get
-this same behavior (servers send the 303 status code to redirect a client's POST request to be followed
-with a GET request). 
-
-HTTP clients should not pipeline requests that have side effects (such as POSTs). In general,
-on error, pipelining prevents clients from knowing which of a series of pipelined requests
-were executed by the server. Because nonidempotent requests such as POSTs cannot safely
-be retried, you run the risk of some methods never being executed in error conditions. 
-
-no repeats
-
-One example of a server extension is Microsoft's FrontPage Server Extension (FPSE), which supports
-web publishing services for FrontPage authors. FPSE is able to interpret remote procedure call (RPC)
-commands sent by FrontPage clients. These commands are piggybacked on HTTP (specifically,
-overlaid on the HTTP POST method). For details, see Section 19.1. 
-
-When a user issues a query to a web search-engine gateway, she fills out an HTML form and her
-browser sends the form to the gateway, using an HTTP GET or POST request. The gateway program
-extracts the search query and converts the web UI query into the expression used to search the full-text
-index.[27] 
-
-
+imagine I send five chunks. this can all come from the same client. like imagine a chaotic mess of letters I need to send
+each letter is send IN THE RIGHT ORDER.
+so these five elters ahve all different adresses, i need to store this adress, and as soon as I get the first letter. I say "ok, start this file."
+this way I don't delte the file if another letter comes in.
 */
