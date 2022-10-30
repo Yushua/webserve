64b2dd98ebf421c361359d20e884f7e8f8d08b86
Yushua <yushabakker1@gmail.com>
2022-10-31 00:28:14 +0100

    included the parts for CGI_post, but my idea did not work. need to look into why not.

--- webserv/src/webserv/CGI/cgi_post.cpp
+++ webserv/src/webserv/CGI/cgi_post.cpp
@@ -4,10 +4,16 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-//check fi this body is chunked
+//check if this body is chunked
 
+#define IS_NOT_CHILD fork_res != 0
+
+//look into how other people do it, it seems the original plan went wrong
+//look into how cgi actually works with the input
+//you cna input part by part, storing the entire message into one string, but how
+//does POST CGI actually handle it?
+//how does pipe nand fork work in this matter
 void webserv::cgi_post(std::string string, const int index, message &msg) {
-
 	std::string boundary = "--" + string;
 	std::cout << "hey[" << boundary  << "]" << std::endl;
 
@@ -19,8 +25,32 @@ void webserv::cgi_post(std::string string, const int index, message &msg) {
 	// int end = 0;
 	std::ifstream infile("root/cgo-bin/cgi-test.txt");
 	std::string line;
+
+	//loop must end when the last post message has been uploaded
 	// while (end == 0){
 	// 	if (std::getline(infile, line) == boundary)
+	// }
+	// while (true){
+	// 	int fds[2];
+	// 	if (pipe(fds) != 0)
+	// 		ft_error("cgi_get");
+		
+	// 	int fork_res = fork();
+
+	// 	if (fork_res == -1)
+	// 		ft_error("fork");
+
+	// 	if (IS_NOT_CHILD) {
+	// 		return;
+	// 	}
+	// 	else {
+	// 		//making argv
+
+	// 		//making envp
+		
+	// 		//execve
+	// 		exit(1);
+	// 	}
 	// }
 	return ;
 }
