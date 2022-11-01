a7487e8163b1d9a6a0ddcaf330d4573640d1490b
Yushua <yushabakker1@gmail.com>
2022-11-01 15:07:55 +0100

    one loop works

--- webserv/src/webserv/CGI/cgi_post.cpp
+++ webserv/src/webserv/CGI/cgi_post.cpp
@@ -22,47 +22,61 @@ void webserv::cgi_post(const int index, const message &msg, const string &reques
     int output_pip[2];
     if (pipe(output_pip) != 0)
         ft_error("cgi_post output");
+	boundary = "--" + boundary;
 	int posa = 0;
 	int posb = 0;
 	std::string str = "";
-	posa = posb + boundary.length() + 1;
+	posa = posb + boundary.length() + 2;
 	posb = msg.getBody().find(boundary, posa);
-	// std::cout << RED << msg.getBody().substr(posa, posb) << RESET << std::endl;
-	int tmp;
-	tmp = index;
-	std::string tmp1 = requested_file;
-	std::string tmp2 = interpreter;
-	// while (true){
-		// int fork_res = fork();
-		// if (fork_res == -1)
-		// 	ft_error("fork");
-		// /* Is not child */
-		// if (fork_res != 0) {
-		// 	close(output_pip[1]);
-		// 	close(input_pipe[0]);
-		// 	close(input_pipe[1]);
-		// 	fcntl(output_pip[0], O_NONBLOCK);
-		// 	this->send_new(index, "HTTP/1.1 200 OK\n", output_pip[0]);
-		// 	sockets_info[index].disconnect_after_send = true;
-		// 	return;
-		// }
-		// else {
-		// 	const vector<string> &args = msg.getArguments();
-		// 	const char *argv[3] = {
-		// 		interpreter != ""
-		// 			? interpreter.c_str()
-		// 			: requested_file.c_str(),
-		// 		requested_file.c_str(),
-		// 		NULL};
-		// 	const char *envp[1] = { NULL };
-		// 	dup2(output_pip[1], 1);
-		// 	dup2(input_pipe[0], 0);
-		// 	// for (string arg in msg.getArguments()) {
-		// 	// 	write(input_pipe[1], arg + '\n');
-		// 	// }
-		// 	write(input_pipe[1], str.c_str(), str.length());
-		// 	cerr << execve(argv[0], (char * const *)argv, (char * const *)envp) << '\n';
-		// 	exit(1);
-		// }
-	// }
+	std::cout << posb << std::endl;
+	std::cout << GREEN << "[" << boundary << "]" << RESET << std::endl;
+	std::cout << GREEN << "[" << msg.getBody().substr(posa, posb) << "]" << RESET << std::endl;
+	//check if the end bondary is in there
+	if (msg.getBody().substr(posa, posb).find( boundary + "--") != string::npos){
+		//did not find the end boundary
+		posb = posb - (boundary.length() + 2);
+		std::cout << RED << "[" << msg.getBody().substr(posa, posb)   << "]" << RESET << std::endl;
+	}
+	else if (msg.getBody().substr(posa, posb).find(boundary) != string::npos){
+		posb = posb - boundary.length() - 2;
+		std::cout << YELLOW << "[" << msg.getBody().substr(posa, posb)  << "]" << RESET << std::endl;
+	}
+	// std::cout << RED << "[" << msg.getBody() << "]" << RESET << std::endl;
+	// int tmp;
+	// tmp = index;
+	// std::string tmp1 = requested_file;
+	// std::string tmp2 = interpreter;
+	while (true){
+		int fork_res = fork();
+		if (fork_res == -1)
+			ft_error("fork");
+		/* Is not child */
+		if (fork_res != 0) {
+			close(output_pip[1]);
+			close(input_pipe[0]);
+			close(input_pipe[1]);
+			fcntl(output_pip[0], O_NONBLOCK);
+			this->send_new(index, "HTTP/1.1 200 OK\n", output_pip[0]);
+			sockets_info[index].disconnect_after_send = true;
+			return;
+		}
+		else {
+			const vector<string> &args = msg.getArguments();
+			const char *argv[3] = {
+				interpreter != ""
+					? interpreter.c_str()
+					: requested_file.c_str(),
+				requested_file.c_str(),
+				NULL};
+			const char *envp[1] = { NULL };
+			dup2(output_pip[1], 1);
+			dup2(input_pipe[0], 0);
+			// for (string arg in msg.getArguments()) {
+			// 	write(input_pipe[1], arg + '\n');
+			// }
+			write(input_pipe[1], str.c_str(), str.length());
+			cerr << execve(argv[0], (char * const *)argv, (char * const *)envp) << '\n';
+			exit(1);
+		}
+	}
 }
