712e790bc5a6fc6c599251b816979711100d10f9
Yushua <yushabakker1@gmail.com>
2022-11-01 16:43:28 +0100

    cgi_post is amsot done, just syntax error due to py, and upload by refrehing gives a long error

--- webserv/src/webserv/CGI/cgi_post.cpp
+++ webserv/src/webserv/CGI/cgi_post.cpp
@@ -27,20 +27,43 @@ void webserv::cgi_post(const int index, const message &msg, const string &reques
 	int posb = 0;
 	std::string str = "";
 	bool loop = true;
+	ofstream file;
+	if (!msg.getStatState())//need to check if this is a chunk file, meaning it exist because a chunk has been send there
+	{
+		//if its folder
+		if (S_ISDIR(msg.getStat().st_mode)){
+			this->send_new_error(sockets[index].fd, 404);
+			this->disconnect(index);
+			return;
+		}
+		//when it does not exist, create
+		file.open(msg.getPath(), fstream::in | fstream::out | fstream::trunc);
+		if (!file.good()){
+			this->send_new_error(sockets[index].fd, 404);
+			this->disconnect(index);
+			return;
+		}
+		file.close();
+	}
+	else{//if fiel does exist, check if the path is not a directory
+		if (S_ISDIR(msg.getStat().st_mode)){
+			this->send_new_error(sockets[index].fd, 404);
+			this->disconnect(index);
+			return;
+		}
+	}
+	std::cout <<"file successfull\n";
 	while (loop == true){
 		posa = posb + boundary.length() + 2;
 		posb = msg.getBody().find(boundary, posa);
-		std::cout << posb << std::endl;
 		//check if the end bondary is in there
 		if (msg.getBody().substr(posa, posb).find( boundary + "--") != string::npos){
 			//did not find the end boundary
 			posb = posb - (boundary.length() + 2);
-			// std::cout << RED << "[" << msg.getBody().substr(posa, posb)   << "]" << RESET << std::endl;
 			loop = false;
 		}
 		else if (msg.getBody().substr(posa, posb).find(boundary) != string::npos){
 			posb = posb - boundary.length() - 2;
-			// std::cout << YELLOW << "[" << msg.getBody().substr(posa, posb)  << "]" << RESET << std::endl;
 		}
 		int fork_res = fork();
 		if (fork_res == -1)
@@ -51,8 +74,27 @@ void webserv::cgi_post(const int index, const message &msg, const string &reques
 			close(input_pipe[0]);
 			close(input_pipe[1]);
 			fcntl(output_pip[0], O_NONBLOCK);
-			this->send_new(index, "HTTP/1.1 200 OK\n", output_pip[0]);
-			sockets_info[index].disconnect_after_send = true;
+			int file_fd = open(msg.getPath().c_str(), O_RDWR | O_APPEND);
+			if (file_fd == -1) {
+				this->send_new_error(sockets[index].fd, 404);
+				this->disconnect(index);
+				return;
+			}
+			int position = this->tryGetAvailablePosition();
+			if (position == -1)
+				ft_error("cgi_post");
+			sockets[position].fd = file_fd;
+			sockets[position].events = POLLOUT;
+			sockets[position].revents = 0;
+			sockets_info[position].disconnect_after_send = true;
+			sockets_info[position].listen = false;
+			sockets_info[position].fd_only = false;
+			write(file_fd
+			, msg.getBody().substr(posa, posb).c_str()
+			, msg.getBody().substr(posa, posb).length());
+			close(file_fd);
+			this->send_new(position, "HTTP/1.1 200 OK\n", output_pip[0]);
+			std::cout <<"the end\n";
 			return;
 		}
 		else {
@@ -64,19 +106,24 @@ void webserv::cgi_post(const int index, const message &msg, const string &reques
 				requested_file.c_str(),
 				NULL};
 			const char *envp[1] = { NULL };
+			std::cout <<"[1]\n";
 			dup2(output_pip[1], 1);
 			dup2(input_pipe[0], 0);
-			map<string, string>::iterator itr = msg.getArguments().begin();
-			map<string, string>::iterator end = msg.getArguments().end();
-			for (; itr < end; itr++){
-				write(input_pipe[1], arg + '\n');
+			size_t i = 0;
+			size_t len = args.size();
+			std::cout <<"[2]\n";
+			for (; i < len; ++i) {
+				write(input_pipe[1], args[i].c_str(), args[i].length());
+				write(input_pipe[1], "\n", 1);
 			}
-			// for (string arg in msg.getArguments()) {
-			// 	write(input_pipe[1], arg + '\n');
-			// }
-			write(input_pipe[1], msg.getBody().substr(posa, posb).c_str(), msg.getBody().substr(posa, posb).c_str());
-			cerr << execve(argv[0], (char * const *)argv, (char * const *)envp) << '\n';
+			std::cout <<"[3]\n";
+			write(input_pipe[1]
+			, msg.getBody().substr(posa, posb).c_str()
+			, msg.getBody().substr(posa, posb).length());
+			std::cerr << execve(argv[0], (char * const *)argv, (char * const *)envp) << '\n';
+			std::cout <<"the end1\n";
 			exit(1);
 		}
 	}
+	std::cout <<"the end11\n";
 }
