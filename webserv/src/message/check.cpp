80d517cc792449dc953f0c12b42d5fc274d47d35
Yushua <yushabakker1@gmail.com>
2022-11-10 15:31:20 +0100

    moving files in parts, instead of changing everything in one go

--- webserv/src/message/check.cpp
+++ webserv/src/message/check.cpp
@@ -1,52 +1,5 @@
 #include <message.hpp>
 
-//each msg has their own bool NOW
-#include <iostream>
-#include <string>
-#include <ctype.h>
-#include <fcntl.h>
-
-void message::checkGet()
-{
-	map<string, string>::iterator itr = headers.begin();
-	map<string, string>::iterator end = headers.end();
-	for (; itr != end; ++itr) {
-		if (itr->first == "Host:") {
-			this->unHost(itr->second);
-		}
-		else if (itr->first == "Referer") {
-			this->unReferer(itr->second);
-		}
-	}
-}
-
-void message::checkPost()
-{
-	map<string, string>::iterator itr = headers.begin();
-	map<string, string>::iterator end = headers.end();
-
-	for (; itr != end; ++itr) {
-		if (itr->first == "Content-Length:" && checkNumber(itr->second, "0123456789")) {
-			this->contentLength = atoi(itr->second.c_str());
-		}
-		else if (itr->first == "Host:") {
-			this->unHost(itr->second);
-		}
-	}
-}
-
-void message::checkDelete()
-{
-	map<string, string>::iterator itr = headers.begin();
-	map<string, string>::iterator end = headers.end();
-	for (; itr != end; ++itr) {
-		if (itr->first == "Host:") {
-			this->unHost(itr->second);
-		}
-	}
-
-}
-
 void message::check()
 {
 	vector<std::string>::iterator itr_v = startLine.begin();
@@ -55,8 +8,6 @@ void message::check()
 	{
 		if (*itr_v == "GET")
 			checkGet();
-		else if (*itr_v == "POST")
-			checkPost();
 		else if (*itr_v == "DELETE")
 			checkDelete();
 	}
