ddddc7ba4572de1a7b88d22be3115ce39403ffa0
LANIK2000 <lanikmail@gmail.com>
2022-11-11 15:59:25 +0100

    Removed Content-Length in check.cpp

--- webserv/src/message/check.cpp
+++ webserv/src/message/check.cpp
@@ -2,27 +2,8 @@
 #include <string>
 #include <iostream>
 
-void message::checkPost()
-{
-	map<string, string>::iterator itr = headers.begin();
-	map<string, string>::iterator end = headers.end();
-
-	for (; itr != end; ++itr) {
-		if (itr->first == "Content-Length:" && checkNumber(itr->second, "0123456789"))
-			{ this->contentLength = atoi(itr->second.c_str());
-		}
-	}
-}
-
 void message::check()
 {
-	vector<std::string>::iterator itr_v = startLine.begin();
-	vector<std::string>::iterator end_v = startLine.end();
-	for (; itr_v < end_v; itr_v++)
-	{
-		if (*itr_v == "POST")
-			checkPost();
-	}
 	map<string, string>::iterator itr = headers.begin();
 	map<string, string>::iterator end = headers.end();
 	for (; itr != end; ++itr)
